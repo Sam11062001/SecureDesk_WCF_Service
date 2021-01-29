67963a43636ef095c2c180c69b3257dd48c8c318
Saurav <sauravladdha83@gmail.com>
2021-01-29 22:16:35 +0530

    Authentication Service is Implemented

--- SecureDesk_WCF_Service/Algorithms/Firebase_Configuration.cs
+++ SecureDesk_WCF_Service/Algorithms/Firebase_Configuration.cs
@@ -2,31 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using FireSharp.Config;
-using FireSharp.Interfaces;
-using FireSharp.Response;
+using Google.Cloud.Firestore;
 
-/* To connect to the firesharp databse we are using the Firesharp Nuget package
- * Firebase.Config: Gives all the code required for the configuration to the Firebase
- * Firebase.Interfaces: Provides all the Methods to work with the Firebase database
- * Firebase.Response : Helps in getting the response from the Firebase
- */
 namespace SecureDesk_WCF_Service
 {
+    
     public class Firebase_Configuration
     {
-        //Authentication to the  Firebase Database we need the FireBaseConfig Class
-        //BasePath and Auth Secret is required to connect with the Firebase database
-        IFirebaseConfig firebase_config = new FirebaseConfig()
-        {
-            AuthSecret = "tyMsKyGB9wbvm6aBy5z5IaYGpKdwJUlxVq9KqyVK",
-            BasePath = "https://desk-ed328-default-rtdb.firebaseio.com/"
-        };
-        public IFirebaseClient Configure()
-        {
-            IFirebaseClient firebaseClient = new FireSharp.FirebaseClient(firebase_config);
-            return firebaseClient;
+        FirestoreDb db;
 
+        public FirestoreDb connectFireStoreCloud()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"deskcloud-155bf-firebase-adminsdk-htpcm-c5324a5466.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            db = FirestoreDb.Create("deskcloud-155bf");
+
+            return db;
         }
+
+
     }
 }
