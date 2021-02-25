3cf0bbbcbfe5ee9460ddfa1996e11881bb03b32f
ykamani <68771216+ykamani@users.noreply.github.com>
2021-02-25 22:04:47 +0530

    Document and diary fetching completed

---
+++ SecureDesk_WCF_Service/Models/DBDiaryData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Google.Cloud.Firestore;
+
+
+namespace SecureDesk_WCF_Service.Models
+{
+    [FirestoreData]
+    public class DBDiaryData
+    {
+        [FirestoreProperty]
+        public string date { get; set; }
+
+        [FirestoreProperty]
+        public string title { get; set; }
+
+        [FirestoreProperty]
+        public string fileLink { get; set; }
+    }
+}
